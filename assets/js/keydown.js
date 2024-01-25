e62a509d152a78c3079c5caa8b51790e28449b4a
Virginia Rojas <liveandletdieeeee@gmail.com>
2024-01-25 17:01:17 -0300

    borrar version antigua

--- assets/js/keydown.js
+++ assets/js/keydown.js
@@ -32,38 +32,3 @@ document.addEventListener('keydown', function(event){
     
 
 
-
-/*
-document.addEventListener('keydown', function(event){
-    if(event.key === 'a' || event.key === 'A'){
-        fondo = 'pink';
-        cambiarColor('#key', fondo);
-    } else if(event.key === 's' || event.key === 'S'){
-        fondo = 'orange';
-        cambiarColor('#key', fondo);
-    } else if(event.key === 'd' || event.key === 'D'){
-        fondo = 'lightblue';
-        cambiarColor('#key', fondo);
-    } else if(event.key === 'q' || event.key === 'Q'){
-        fondo = 'purple';
-        agregarElemento();
-    } else if(event.key === 'w' || event.key === 'W'){
-        fondo = 'gray';
-        agregarElemento();
-    } else if(event.key === 'e' || event.key === 'E'){
-        fondo = 'brown';
-        agregarElemento();
-    }
-});
-
-function agregarElemento() {
-    // Obtenemos el elemento contenedor por su ID
-    contenedor = document.getElementById("container");
-    // Creamos un nuevo elemento <div>
-    nuevoElemento = document.createElement("div");
-    // Asignamos un texto al nuevo elemento
-    nuevoElemento.textContent = "Elemento agregado";
-    // Agregamos el nuevo elemento al contenedor
-    contenedor.appendChild(nuevoElemento);
-    }
-    */
